4aaf8c8e7505d13686fd8cfed1481dab349b2be0
SaanviPatankar624 <75069021+SaanviPatankar624@users.noreply.github.com>
2020-12-14 15:32:27 +0530

    Add files via upload

---
+++ Pinkblock.js
@@ -0,0 +1,22 @@
+class Pinkblock extends Block{
+    constructor(x, y){
+    super(x, y, 20, 30);
+    this.Visibility = 255;
+    }
+    display(){
+        console.log(this.body.speed);
+       
+       if(this.body.speed < 3){
+          super.display();
+       }
+       else{
+        World.remove(world, this.body);
+        push()
+        this.Visibility = this.Visibility -5;
+        tint(255, this.Visibility);
+        rect(this.body.position.x, this.body.position.y, 50, 50);
+        fill("pink");
+        pop();
+    }
+  }
+};
